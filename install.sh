1f385078fa1f3d774e8259a50d06c4e62d8d14a7
Tristan Le Guern <tleguern@bouledef.eu>
2024-11-04 10:33:22 +0100

    Finally create an install script

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+careful() {
+	file="$1"; shift
+
+	if ! diff -q "$file" ~/."$file"; then
+		diff -u "$file" ~/."$file"
+		echo -n "Replace ? "
+		read REPLY
+		case "$REPLY" in
+		Y) cp "$file" ~/."$file";;
+		*) .;;
+		esac
+	fi
+}
+
+mkdir -p ~/.ssh
+
+case "$(uname -s)" in
+	OpenBSD)
+		cp -r calendar ~/.calendar
+		cp -r config ~/.config
+		careful cwmrc
+		if [ ! -f ~/.ssh/local ]; then
+			echo 'IdentityAgent ~/.ssh/agent.sock' > ~/.ssh/local
+		fi
+		careful wsconsctl.conf
+		careful Xdefaults
+		careful xinitrc
+		careful xpdfrc
+		careful xsession
+	;;
+	Linux)
+		careful mkshrc
+		if [ ! -f ~/.ssh/local ]; then
+			touch ~/.ssh/local
+		fi
+		chmod -w ~/.mkshrc
+		rm -f .bash_history
+		rm -f .bash_logout
+		rm -f .bashrc
+	;;
+	*) echo "Hu?"; exit 1;;
+esac
+
+careful gitconfig
+careful kshrc
+cp nethackrc ~/.nethackrc
+cp ssh/config ~/.ssh/
+careful vimrc
+
+chmod -w ~/.ssh/config
+chmod -w ~/.kshrc
